540038555912304335e6a1322e51c3a8a315e083
ibiscum <ulf.luecking@gmail.com>
2026-02-20 23:14:14 +0100

    ch08

--- Chapter06/06-web-approach/run.sh
+++ Chapter06/06-web-approach/run.sh
@@ -1,2 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 (trap 'kill 0' SIGINT; (cd mf-gw && npm start) & (cd mf-1 && npm start) & (cd mf-2 && npm start))
--- Chapter07/07-server-side-composition/run.sh
+++ Chapter07/07-server-side-composition/run.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/usr/bin/env bash
 (trap 'kill 0' SIGINT; (cd red && npm start) & (cd blue && npm start) & (cd green && npm start) & (cd gateway && npm start))
