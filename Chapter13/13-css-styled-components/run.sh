62dc49a352eb8ea0eb39b6aa538721440b65c934
ibiscum <ulf.luecking@gmail.com>
2026-02-21 06:49:35 +0100

    ch13

--- Chapter13/13-css-styled-components/run.sh
+++ Chapter13/13-css-styled-components/run.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-cd mf-blue
-npm i
+cd mf-blue || exit
+pnpm i
 cd ..
-cd mf-red
+cd mf-red || exit
 npm i
 npx pilet debug ../mf-red/src/index.tsx ../mf-blue/src/index.tsx
