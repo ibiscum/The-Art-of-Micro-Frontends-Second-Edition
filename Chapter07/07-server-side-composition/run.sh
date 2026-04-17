2b4f12f7eb1b9412503b900704773a7960abbf45
ibiscum <ulf.luecking@gmail.com>
2026-04-17 22:33:10 +0200

    update

--- Chapter07/07-server-side-composition/run.sh
+++ Chapter07/07-server-side-composition/run.sh
@@ -3,5 +3,5 @@
 (trap 'kill 0' SIGINT; 
 (cd red && pnpm run start) & 
 (cd blue && pnpm run start) & 
-(cd green && pnpm start) & 
-(cd gateway && npm start))
+(cd green && pnpm run start) & 
+(cd gateway && pnpm run start))
